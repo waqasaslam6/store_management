e55f7fd73e85499522436b6f43560251c529524e
bushart hussain <bushart@zestlog.com>
2020-12-18 18:40:18 +0500

    login page design

--- lib/pages/Login.dart
+++ lib/pages/Login.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -10,79 +9,109 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff313c50),
-      body: Column(
-        children: [
-          Expanded(child: SizedBox(),),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(children: <Widget>[
           Container(
-            padding: EdgeInsets.only(left: 20,right: 20,top: 15),
-            margin: EdgeInsets.only(top: 20,left: 30,right: 20),
-            height: MediaQuery.of(context).size.height/2,
+            height: MediaQuery.of(context).size.height / 2.5,
             width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              color: Colors.white,
+            decoration: const BoxDecoration(
+              color: Colors.green,
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(100)),
             ),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text("Welcome!",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
-                ),),
-                Text("Login to continue",
-                  style: TextStyle(
-                    fontSize: 14,
-                  ),),
-                 TextFormField(
-                   decoration: InputDecoration(
-                     prefixIcon: Icon(Icons.person),
-                     hintText: "Email",
-                     border: OutlineInputBorder()
-                   ),
-                 ),
-
-                TextFormField(
-                  decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.lock),
-                    hintText: "Password",
-                      border: OutlineInputBorder()
+              children: <Widget>[
+                const Padding(
+                  padding: const EdgeInsets.only(top: 50),
+                  child: CircleAvatar(
+                    radius: 85,
+                    backgroundColor: Colors.transparent,
+                    backgroundImage: AssetImage('images/user.png'),
                   ),
                 ),
-                Text("Forgot password?"),
-                Row(
-                  children: [
-                    Expanded(
-                      child: MaterialButton(
-                        height: 45,
-                        color: Colors.red,
-                        onPressed: (){},
-                        child: Text("Login",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16
-                        ),),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(40)),
-                        ),
-                      ),
+                const Padding(
+                  padding: const EdgeInsets.only(left: 270),
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35,
                     ),
-                  ],
-                ),
-
+                  ),
+                )
               ],
             ),
           ),
-          Expanded(child: SizedBox(),),
-          Text("New User ? Signup",
-          style: TextStyle(
-            color: Colors.white,
-          ),),
-          SizedBox(height: 15,),
-        ],
+          const SizedBox(height: 25),
+          Padding(
+            padding: const EdgeInsets.only(left: 25, right: 25),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                  hintText: 'User name',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30))),
+                  prefixIcon: Icon(Icons.person)),
+            ),
+          ),
+          SizedBox(height: 25),
+          Padding(
+            padding: const EdgeInsets.only(left: 25, right: 25),
+            child: TextFormField(
+              decoration: InputDecoration(
+                hintText: "Password",
+                prefixIcon: Icon(Icons.lock),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30))),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 220),
+            child: Text(
+              "Forget Password?",
+              style:
+              TextStyle(color: Colors.green, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(height: 20),
+          MaterialButton(
+            minWidth: 300,
+            height: 45,
+            color: Colors.green,
+            onPressed: () {
+              Navigator.pushNamed(context, '/db');
+            },
+            child: Text(
+              "Login",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+            ),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(30))),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text(
+              "Don't have an accont?",
+              style:
+              TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "  Register",
+              style:
+              TextStyle(color: Colors.green, fontWeight: FontWeight.bold),
+            ),
+          ]),
+        ]),
       ),
-
     );
   }
 }
